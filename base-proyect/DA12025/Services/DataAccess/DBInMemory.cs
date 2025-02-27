467dbc7d5a0f24e14d2eb8cc6c6f58c16ef3c34d
marcos <marcosbuydid@gmail.com>
2025-02-26 23:25:15 -0300

    create public methods on dbinmemory

--- base-proyect/DA12025/Services/DataAccess/DBInMemory.cs
+++ base-proyect/DA12025/Services/DataAccess/DBInMemory.cs
@@ -9,12 +9,48 @@ namespace Services.DataAccess
 {
     public class DBInMemory
     {
-        public List<Movie> Movies { get; set; }
+        private List<Movie> Movies { get; set; }
 
         public DBInMemory()
         {
             Movies = new List<Movie>();
             loadDefaultMovies();
+        }
+
+        public List<Movie> GetAll()
+        {
+            return Movies;
+        }
+
+        public void Add(Movie movie)
+        {
+            Movies.Add(movie);
+        }
+
+        public void Remove(Movie movie)
+        {
+            Movies.Remove(movie);
+        }
+
+        public Movie Get(string title)
+        {
+            Movie movie = Movies.FirstOrDefault(movie => movie.Title == title);
+            if (movie == null)
+            {
+                throw new FileNotFoundException("Cannot find movie with this title");
+            }
+            return movie;
+        }
+
+        public int GetIndex(string title)
+        {
+            var movieIndex = Movies.IndexOf(Movies.Find(m => m.Title == title));
+
+            if (movieIndex == -1)
+            {
+                throw new IndexOutOfRangeException("Cannot find selected index");
+            }
+            return movieIndex;
         }
 
         private void loadDefaultMovies()
