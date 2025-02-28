b0437bfd092c70f065ef460900ff395c6fc3ec38
marcos <marcosbuydid@gmail.com>
2025-02-28 12:00:42 -0300

    refactor validations using data annotations

--- base-proyect/DA12025/Services/Models/Movie.cs
+++ base-proyect/DA12025/Services/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace Services.Models
 {
     public class Movie
     {
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Director is required.")]
         public string Director { get; set; }
         public DateTime ReleaseDate { get; set; }
 
